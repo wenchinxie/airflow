bb02cbb39c4c0018bd9bcac8d05983f092c71063
Xiexieni9527 <wenchin.xie@gmail.com>
2023-07-15 15:54:32 +0800

    [Dag] Add companies and their own industries

--- update_inds_comps.py
+++ update_inds_comps.py
@@ -1,5 +1,4 @@
 import pendulum
-
 from airflow import DAG
 
 
@@ -11,22 +10,25 @@ with DAG(
     schedule_interval="@once",
     start_date=pendulum.datetime(2023, 1, 12, 9, 0, tz="Asia/Taipei"),
     catchup=False,
-    tags=["Industries", "Companies"],
+    tags=["industries", "companies"],
 ) as dag:
     from airflow.operators.python import ExternalPythonOperator
     from airflow.configuration import conf
-    from airflow.plugins import router
+    from router import get_db_conn
 
-    def call_inds_scraper():
+    def call_inds_scraper(get_db_conn_fnuc):
         from Financial_data_crawler.DataReader import HTTPClient
 
         inds_df = HTTPClient.IndScraper().scrape_data()
 
-        mysql_conn = router.get_mysql_conn()
+        mysql_conn = get_db_conn_fnuc("mysql_address")
         inds_df.to_sql("Inds", mysql_conn, if_exists="replace")
 
     save_inds_comps_to_db = ExternalPythonOperator(
-        task_id="",
+        task_id="save_industries_info",
         python=conf.get("core", "virtualenv"),
         python_callable=call_inds_scraper,
+        op_args=[get_db_conn],
     )
+
+    save_inds_comps_to_db
