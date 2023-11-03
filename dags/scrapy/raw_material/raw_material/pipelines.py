2a673c4afe759ce09605995afd6133f72029f9c2
Xiexieni9527 <wenchin.xie@gmail.com>
2023-11-04 00:23:44 +0800

    [Refactor] create pipeline

--- dags/scrapy/raw_material/raw_material/pipelines.py
+++ dags/scrapy/raw_material/raw_material/pipelines.py
@@ -1,13 +1,21 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
+from mongoengine import DateTimeField, StringField, FloatField, Document
 from itemadapter import ItemAdapter
+
+
+class RawMaterial(Document):
+    date = DateTimeField()
+    material_Name = StringField()
+    price = FloatField()
+
+    meta = {
+        "shard_key": ("date", "material_name"),
+        "indexes": ("date", "material_name"),
+    }
 
 
 class RawMaterialPipeline:
     def process_item(self, item, spider):
+        item_dict = ItemAdapter(item).asdict()
+        raw_material = RawMaterial(**item_dict)
+        raw_material.save()
         return item
