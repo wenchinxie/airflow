9a2ad12a76b29c0d7f8864dfc362c4c9aa166ac5
Xiexieni9527 <wenchin.xie@gmail.com>
2023-11-04 00:33:38 +0800

    [Refactor] change name in case duplicated with folder

---
+++ dags/scrapy_crawler/parse_raw_material.py
@@ -0,0 +1,15 @@
+import pendulum
+from airflow import DAG
+
+
+with DAG(
+    "scrapy__parse_raw_material_from_sci",
+    schedule="0 21 */5 * *",
+    start_date=pendulum.datetime(2023, 1, 12, 9, 0, tz="Asia/Taipei"),
+    catchup=False,
+    tags=["Scarpy", "Raw Material"],
+) as dag:
+
+    def parse_sci():
+        from crawler.raw_material.raw_material.spiders.sci import SciSpider
+        from scrapy.crawler import CrawlerProcess
