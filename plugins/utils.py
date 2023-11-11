9cb73c61329112b8b91c37acdf0a55dc38ab1bb9
Xiexieni9527 <wenchin.xie@gmail.com>
2023-11-11 10:39:17 +0800

    [Minor] fix typo

--- plugins/utils.py
+++ plugins/utils.py
@@ -4,9 +4,8 @@ import os
 from airflow.plugins_manager import AirflowPlugin
 
 
-def add_relatieve_path_to_sys(path: str):
+def add_relative_path_to_sys(relative_path: str):
     # Define your relative path
-    relative_path = "./raw_material/"
 
     # Convert it to an absolute path
     absolute_path = os.path.abspath(relative_path)
@@ -17,4 +16,4 @@ def add_relatieve_path_to_sys(path: str):
 
 class AirflowRouterPlugin(AirflowPlugin):
     name = "utils"
-    macros = [add_relatieve_path_to_sys]
+    macros = [add_relative_path_to_sys]
