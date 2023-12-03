1a2683c7fff5d65059f8b000050739565ddf0948
Xiexieni9527 <wenchin.xie@gmail.com>
2023-12-03 12:44:14 +0800

    create is_market_open

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,5 +1,12 @@
-import sys
+import datetime
 import os
+import sys
+
+import chardet
+import httpx
+from bs4 import BeautifulSoup
+from faker import Faker
+
 from airflow.plugins_manager import AirflowPlugin
 
 
@@ -13,6 +20,31 @@ def add_relative_path_to_sys(relative_path: str):
     sys.path.append(absolute_path)
 
 
+def autodetect(content):
+    return chardet.detect(content).get("encoding")
+
+
+def is_market_open():
+    client = httpx.Client(default_encoding=autodetect)
+    header = Faker().user_agent()
+
+    response = client.get(
+        "https://tw.stock.yahoo.com/quote/2330.TW", headers={"User-Agent": header}
+    )
+    if response.status_code != 200:
+        return False
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    market_day = soup.find("span", {"class": "C(#6e7780) Fz(12px) Fw(b)"}).text
+    market_day = market_day.split(" ")[2]
+
+    if market_day == "-" or market_day == datetime.datetime.today().strftime(
+        "%Y-%m-%d"
+    ):
+        return True
+    return False
+
+
 class AirflowRouterPlugin(AirflowPlugin):
     name = "utils"
-    macros = [add_relative_path_to_sys]
+    macros = [add_relative_path_to_sys, is_market_open]
