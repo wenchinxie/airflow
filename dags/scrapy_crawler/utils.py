9ed70630ef7273d985260b5cf04695d68c54445e
Xiexieni9527 <wenchin.xie@gmail.com>
2024-01-03 23:32:51 +0800

    [WIP] revise bash script

--- dags/scrapy_crawler/utils.py
+++ dags/scrapy_crawler/utils.py
@@ -1,7 +1,8 @@
 def get_scrapy_crawl_command(folder: str, project: str, spider: str, args: str):
     command = f"""
+    source /root/miniconda3/etc/profile.d/conda.sh &&
     cd /root/crawler/scrapy_crawler/{folder}/{project} &&
-    conda run -n airflow scrapy crawl {spider}  {args}
+    conda activate airflow && scrapy crawl {spider}  {args}
     """
 
     return command
